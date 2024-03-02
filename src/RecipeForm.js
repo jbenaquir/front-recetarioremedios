1ef966cd48e42e25821b981d786ff91012e73f8c
jbenavides_asotea <jbenavides@asotea.com>
2024-03-01 21:10:17 -0500

    check products save win web api linked

--- src/RecipeForm.js
+++ src/RecipeForm.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import RecipeProductsCheck from './RecipeProductsCheck';
 
 function RecipeForm() {
-    const { id } = useParams();
+    let { id } = useParams();
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
     const [preparation, setPreparation] = useState('');
+    const [products, setProducts] = useState([]);
 
     function GetRecipe(id) {
         fetch(`https://localhost:7222/api/Recipes/${id}`,
@@ -51,15 +53,41 @@ function RecipeForm() {
                 },
                 body: JSON.stringify(recipe)
             })
+            .then(response => response.json())
+            .then(data => {
+                debugger;
+                if(saveType === "save"){
+                    id = data.id;//load id
+                }
+                SaveProducts();
+            })
+            .catch((error) => console.log("Something happened saving recipe: " + error));
+    }
+
+    function SaveProducts(){
+        fetch(`https://localhost:7222/api/RecipeProducts/saveForRecipe/${id}`,
+            {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(products)
+            })
             .then(response => {
                 if(response.status === 200)
+                {
                     window.location.href = `/recipes`;
+                }
             })
             .catch((error) => console.log("Something happened saving recipe: " + error));
     }
     
     function Back() {
         window.location.href = `/recipes`;
+    }
+
+    function receiveProducts(values){
+        setProducts(values);
     }
 
     return (
@@ -92,6 +120,11 @@ function RecipeForm() {
                         onChange={ e => setPreparation(e.target.value)} 
                         maxLength={100}/>
                 </label>
+            </div>
+            <div>
+                <RecipeProductsCheck 
+                    recipeId={id}
+                    sendProducts={receiveProducts} />
             </div>
             <div>
                 <button onClick={Save}>Save</button>
---
+++ src/RecipeProductsCheck.js
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react";
+
+function RecipeProductsCheck ({recipeId, sendProducts}) {
+    const [products, setProducts] = useState([]);
+    const [recipeProducts, setRecipeProducts] = useState([]);
+
+    function GetProducts(){
+        fetch(`https://localhost:7222/api/Products/search`,
+            {
+                method: 'GET'
+            })
+            .then(response => response.json())
+            .then(data => {
+                setProducts(data);
+            })
+            .catch((error) => console.log("Something happened getting product: " + error));
+    }
+
+    function GetProductsByRecipe(){
+        if(!recipeId) return;
+
+        fetch(`https://localhost:7222/api/RecipeProducts/getByRecipe/${recipeId}`,
+            {
+                method: 'GET'
+            })
+            .then(response => response.json())
+            .then(data => {
+                setRecipeProducts(data);
+            })
+            .catch((error) => console.log("Something happened getting product: " + error));
+    }
+
+    function CheckProduct() {
+        const products = Array.from(document.querySelectorAll('[name="chkProduct"]:checked')).map(chk => chk.value);
+        //return to parent
+        sendProducts(products);
+    }
+
+    useEffect(() => {
+        GetProducts();
+        GetProductsByRecipe();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[recipeId]);
+
+    return (
+        <>
+            <h3>Products:</h3>
+            <table>
+                <tbody>
+                {products.map(product => (
+                    <tr key={product.id}>
+                        <td>
+                            <label htmlFor={`chkProduct${product.id}`}>{product.name}</label>
+                        </td>
+                        <td>
+                            <input 
+                                type="checkbox" 
+                                name="chkProduct" 
+                                id={`chkProduct${product.id}`} 
+                                value={product.id} 
+                                defaultChecked={recipeProducts.some(rP => rP.name === product.name)}
+                                onChange={()=>CheckProduct()}
+                                />
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </>
+    );
+}
+
+export default RecipeProductsCheck;
